3d30ad567698acf777e1d3f95b91f37f1e903b76
Alex_Des <alexis.desforges@icloud.com>
2023-11-21 18:36:27 -0500

    Small changes

--- myscript.py
+++ myscript.py
@@ -1,8 +1,8 @@
 import os
 
-BAD_HASH            = 'c1a4be04b972b6c17db242fc37752ad517c29402'
-GOOD_HASH           = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
+BAD_COMMIT = 'c1a4be04b972b6c17db242fc37752ad517c29402'
+GOOD_COMMIT = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
 
-os.system(f'git bisect start {BAD_HASH} {GOOD_HASH}')
+os.system(f'git bisect start {BAD_COMMIT} {GOOD_COMMIT}')
 os.system('git bisect run python manage.py test')
 os.system('git bisect reset')
