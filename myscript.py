eb26af2c6be2eb59be287b9ec03c4f8015e7f905
Alex_Des <alexis.desforges@icloud.com>
2023-11-21 18:40:11 -0500

    Fixed bug and myscript.py

--- myscript.py
+++ myscript.py
@@ -1,8 +1,5 @@
 import os
 
-BAD_COMMIT = 'c1a4be04b972b6c17db242fc37752ad517c29402'
-GOOD_COMMIT = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
-
-os.system(f'git bisect start {BAD_COMMIT} {GOOD_COMMIT}')
+os.system(f'git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
 os.system('git bisect run python manage.py test')
 os.system('git bisect reset')
